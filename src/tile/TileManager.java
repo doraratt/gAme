748f4530df585f65b5e053bc000362700478f2f6
El Semilla <ella.semilla28@gmail.com>
2024-04-23 00:47:35 +0800

    fourth

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -5,7 +5,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import javax.imageio.ImageIO;
 import main.gamePanel;
 
@@ -16,13 +19,17 @@ import main.gamePanel;
 public class TileManager {
     gamePanel gPanel;
     Tile[] tile;
+    int mapTileNum[][];
+    
     
     public TileManager(gamePanel gPanel){
         this.gPanel = gPanel;
         
         tile = new Tile[10];
+        mapTileNum = new int[gPanel.maxScreenCol][gPanel.maxScreenRow];
         
         getTileImage();
+        loadMap();
         
     }
     
@@ -40,11 +47,42 @@ public class TileManager {
             e.printStackTrace();
         }
     }
+    public void loadMap(){
+        try{
+            InputStream is = getClass().getResourceAsStream("/maps/map01.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            
+            int col=0, row=0;
+            
+            while(col<gPanel.maxScreenCol && row<gPanel.maxScreenRow){
+                String line = br.readLine();
+                
+                while(col<gPanel.maxScreenCol){
+                    String numbers[] = line.split(" ");
+                    
+                    int num = Integer.parseInt(numbers[col]);
+                    
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if(col==gPanel.maxScreenCol){
+                    col = 0;
+                    row++;
+                }
+               
+            }
+             br.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     public void draw(Graphics2D g2){
         int col=0, row=0, x=0, y=0;
         
         while(col<gPanel.maxScreenCol && row<gPanel.maxScreenRow){
-            g2.drawImage(tile[0].image, x, y, gPanel.tileSize, gPanel.tileSize, null);
+            int tileNum = mapTileNum[col][row];
+            
+            g2.drawImage(tile[tileNum].image, x, y, gPanel.tileSize, gPanel.tileSize, null);
             col++;
             x += gPanel.tileSize;
             
